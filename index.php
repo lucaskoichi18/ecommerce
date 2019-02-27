b00ddaf69273d2e88dd5591f9765329331bf7445
lucaskoichi18 <47890037+lucaskoichi18@users.noreply.github.com>
2019-02-27 15:01:57 -0300

    recuperar senha

--- index.php
+++ index.php
@@ -146,6 +146,34 @@ $app->post("/admin/users/:iduser", function($iduser){
 
 });
 
+$app->get('/admin/forgot', function(){
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("/forgot");
+});
+
+$app->post("/admin/forgot", function(){
+
+	$user = User::getForgot($_POST["email"]);
+
+	header("Location: /ecommerce/index.php/admin/forgot/sent");
+	exit;
+});
+
+$app->get("/admin/forgot/sent", function(){
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("/forgot-sent");
+});
+
 $app->run();
 
 ?>
